c555c59703f5c0449628c6ea4943f00fc2a76473
sumito.tsukada <sumito.tsukada@rarejob.co.jp>
2024-02-09 18:04:21 +0900

    refer to env

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -4,4 +4,12 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkEcspressoStack } from '../lib/cdk-ecspresso-stack';
 
 const app = new cdk.App();
-new CdkEcspressoStack(app,'CdkEcspressoStack');
+// 環境変数の読み込み
+export const context = app.node.tryGetContext(app.node.tryGetContext('env'))
+// Add cost ditribution tag on this entire app
+console.log(context)
+cdk.Tags.of(app).add('CmBillingGroup', context.costTagValue)
+
+new CdkEcspressoStack(app, 'CdkEcspressoStack', {
+  env: context.env
+});
